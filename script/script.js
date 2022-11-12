d5530295ca1b0c593a3a36254c3a0ae85842e0ef
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-12 19:46:46 +0530

    ui completed

---
+++ script/script.js
@@ -0,0 +1,22 @@
+`use strict`
+
+const image = document.getElementById(`imgs`);
+const hint = document.getElementById(`hint`);
+
+const gussEl = document.getElementById(`guss`)
+const Submit = document.getElementById(`submit`);
+
+// globle veriable 
+
+
+
+
+
+
+Submit.addEventListener('click',()=>{
+    if(gussEl.value === ''){
+        alert('need to enter smoe thing')
+    }else{
+        
+    }
+})
